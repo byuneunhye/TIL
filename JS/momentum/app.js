2cd15c9b14997609ae6181a572e2dc78091ddf1f
byuneunhye <gracebyun06@gmail.com>
2023-01-10 22:47:19 +0900

    2.13 conditionals

--- JS/momentum/app.js
+++ JS/momentum/app.js
@@ -1,31 +1,8 @@
-const calculater = {
-    plus : function(a, b){
-        return(a + b);
-        console.log("hello"); // return을 하면 모든 함수가 끝남
-    },
-    minus : function(a, b){
-        return(a - b);
-    },
-    mul : function(a, b){
-        return(a * b);
-    },
-    div : function(a, b){
-        return(a / b);
-    },
-    powerof : function(a, b){
-        return "hello";
-    },
-} 
+const age = prompt("How old are you?");
+console.log(age, typeof(age));
+parseInt(age); //정수형으로 type을 변환 해 줌
+console.log(age, typeof(parseInt(age)), parseInt(age)); //"15" => 15 가아니라 nan이네 왜지 => typeof(parseInt(age))로 안 하고parseInt를 밖에 적었어서 그랬음
 
 
-console.log(calculater.plus(4, 5));
-
-
-const plusResult = calculater.plus(2, 3);
-const minusResult = calculater.minus(2,3);
-const mulResuslt = calculater.mul(minusResult, 5);
-const divideResult = calculater.div(mulResuslt, plusResult);
-const powerofResult = calculater.powerof(divideResult, minusResult);
-const powR = calculater.powerof(3, 4); //powerof의 return 값은 hello이므로 인자로 어떤 값을 넣어도 hello를 출력 함
 
 //결과는 브라우저의 콘솔 창에서 확인 할 수 있음
