21698618d24f6c81bcafde42a6b51116dcc56969
byuneunhye <gracebyun06@gmail.com>
2023-01-10 23:27:00 +0900

    2.14 conditionals part two

--- JS/momentum/app.js
+++ JS/momentum/app.js
@@ -1,8 +1,7 @@
-const age = prompt("How old are you?");
-console.log(age, typeof(age));
-parseInt(age); //정수형으로 type을 변환 해 줌
-console.log(age, typeof(parseInt(age)), parseInt(age)); //"15" => 15 가아니라 nan이네 왜지 => typeof(parseInt(age))로 안 하고parseInt를 밖에 적었어서 그랬음
+const age = parseInt(prompt("How old are you?"));
 
-
+if(isNaN(age)){ //isNan: 정수가 아니냐? (is Not a number?)
+    console.log("Please write a number. ");
+}
 
 //결과는 브라우저의 콘솔 창에서 확인 할 수 있음
