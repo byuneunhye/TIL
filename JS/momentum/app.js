ee4ac8b49581375a23a975db78c0cbf733a02562
byuneunhye <gracebyun06@gmail.com>
2023-01-11 00:47:43 +0900

    3.0 The Document Object

--- JS/momentum/app.js
+++ JS/momentum/app.js
@@ -1,16 +1,3 @@
-const age = parseInt(prompt("How old are you?"));
+document.title = "Hello! From JS!";
 
-if(isNaN(age) || age<0){
-    console.log("Please write a real positive number");
-}else if(age < 18){
-    console.log("You are too young");
-}else if(age>=18 && age<=50){
-    console.log("You can drink");
-}else if(age === 100){
-    console.log("wow you are wise");
-}
-else{
-    console.log("You can't drink");
-}
-
-//결과는 브라우저의 콘솔 창에서 확인 할 수 있음
+//javascript에서 html의 항목들을 불러올 수 있다.
