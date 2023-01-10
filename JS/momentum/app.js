8c9f2faabbbb9caebe7b666e025a3701bd2fac19
byuneunhye <gracebyun06@gmail.com>
2023-01-10 16:11:25 +0900

    2.11 returns

--- JS/momentum/app.js
+++ JS/momentum/app.js
@@ -1,26 +1,29 @@
 const calculater = {
     plus : function(a, b){
-        console.log(a + b);
+        return(a + b);
     },
     minus : function(a, b){
-        console.log(a - b);
+        return(a - b);
     },
     mul : function(a, b){
-        console.log(a * b);
+        return(a * b);
     },
     div : function(a, b){
-        console.log(a / b);
+        return(a / b);
     },
     powerof : function(a, b){
-        console.log(a ** b)
+        return(a ** b)
     },
 } 
 
-calculater.plus(3, 4)
-calculater.minus(3, 4)
-calculater.mul(3, 4)
-calculater.div(3, 4)
-calculater.powerof(3, 4)
 
+console.log(calculater.plus(4, 5));
+
+
+const plusResult = calculater.plus(2, 3);
+const minusResult = calculater.minus(2,3);
+const mulResuslt = calculater.mul(minusResult, 5);
+const divideResult = calculater.div(mulResuslt, plusResult);
+const powerofResult = calculater.powerof(divideResult, minusResult);
 
 //결과는 브라우저의 콘솔 창에서 확인 할 수 있음
