9305870dd8a06766bda3336854828180278ea5e3
byuneunhye <gracebyun06@gmail.com>
2023-01-10 23:38:47 +0900

    2.15 conditionals part three

--- JS/momentum/app.js
+++ JS/momentum/app.js
@@ -1,7 +1,13 @@
 const age = parseInt(prompt("How old are you?"));
 
-if(isNaN(age)){ //isNan: 정수가 아니냐? (is Not a number?)
-    console.log("Please write a number. ");
+if(isNaN(age) || age<0){
+    console.log("Please write a real positive number");
+}else if(age < 18){
+    console.log("You are too young");
+}else if(age>=18 && age<=50){
+    console.log("You can drink");
+}else{
+    console.log("You can't drink");
 }
 
 //결과는 브라우저의 콘솔 창에서 확인 할 수 있음
