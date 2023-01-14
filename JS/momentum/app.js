f150e4770fbd103f859fa726825f1a6d321afe2d
byuneunhye <gracebyun06@gmail.com>
2023-01-14 18:14:27 +0900

    3.1 HTML in Javascript

--- JS/momentum/app.js
+++ JS/momentum/app.js
@@ -1,3 +1,12 @@
-document.title = "Hello! From JS!";
+const titleInJS = document.getElementById("title"); //*****title들고오는 거***** 제일중요!!!**
 
-//javascript에서 html의 항목들을 불러올 수 있다.
+console.log(titleInJS);
+console.dir(titleInJS);
+
+titleInJS.innerHTML = "Got you!"; // Grab me -> Got you
+
+console.log(titleInJS.id); //dir에서 id값이 있음 => titleInJS
+console.log(titleInJS.className); //dir에서 className값이 생김 => helllo!
+
+//html이 아닌 js에서도 문서를 수정 할 수 있다. (innerHTML)
+//getElementById로 html에서 쓴 애 데리고 오기, 데리고 온 애로 JS에서 작업하기
