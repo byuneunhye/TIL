948abee31233666a23e3ae3f1266c8d2c4043b99
byuneunhye <gracebyun06@gmail.com>
2023-01-14 22:52:50 +0900

    3.3 Events

--- JS/momentum/app.js
+++ JS/momentum/app.js
@@ -1,7 +1,9 @@
-const hi = document.querySelector(".hello");
+const title = document.querySelector("div.hello:first-child h1");
 
-console.dir(title);
+function handleTitleclick() {
+    console.log("title was clicked!");
+}
 
-hi.style.color = "blue";
+title.addEventListener("click",handleTitleclick); //여기서 handleTitleclick뒤에 괄호()를 안쓰는건 정말 중요함 => 괄호쓰면 바로 실행 됨, 우리는 크릭이벤트가 발생하면 자바스크립트에서 자동으로 실행하기를 기대하는것임.
 
 // js에서 style도 바꿀 수 있다!!
