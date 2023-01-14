d800fc4b0bd4cf182731598b00b198986f210757
byuneunhye <gracebyun06@gmail.com>
2023-01-14 21:32:17 +0900

    3.2 Searching for elements

--- JS/momentum/app.js
+++ JS/momentum/app.js
@@ -8,5 +8,11 @@ titleInJS.innerHTML = "Got you!"; // Grab me -> Got you
 console.log(titleInJS.id); //dir에서 id값이 있음 => titleInJS
 console.log(titleInJS.className); //dir에서 className값이 생김 => helllo!
 
+
+const hiInJS = document.querySelector(".hi h1"); // getElementById를 CSS형태로 가져옴
+console.log(hiInJS);
+
+//querySelector("#title")이랑 getElementById("title")이랑 똑같은거
+
 //html이 아닌 js에서도 문서를 수정 할 수 있다. (innerHTML)
 //getElementById로 html에서 쓴 애 데리고 오기, 데리고 온 애로 JS에서 작업하기
