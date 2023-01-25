54f0195eb972687b63f140d3ebc6584f52451738
byuneunhye <gracebyun06@gmail.com>
2023-01-25 23:19:11 +0900

    3.6 CSS in Javascript

--- JS/momentum/app.js
+++ JS/momentum/app.js
@@ -3,11 +3,14 @@ const h1 = document.querySelector("div.hello:first-child h1");
 //console.dir(h1);
 
 function handletitleclick(){ 
-    if(h1.style.color === "blue"){
-        h1.style.color = "tomato";
+    const currentColor = h1.style.color;
+    let newColor;  //let, 새로 업데이트 될 수 있는 변수 선언 할 때 씀/ 값 할당 안 해줬으므로 비어있는 let변수 선언
+    if(currentColor === "blue"){
+        newColor = "tomato";
     } else {
-        h1.style.color = "blue";
+        newColor = "blue";
     }
+    h1.style.color = newColor;
 }
 
 
