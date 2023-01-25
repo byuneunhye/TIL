34f5dbd44c3204cafac360add4b45746ef6c4d2e
byuneunhye <gracebyun06@gmail.com>
2023-01-25 16:19:39 +0900

    정리

--- JS/momentum/app.js
+++ JS/momentum/app.js
@@ -10,13 +10,13 @@ function handleTitleclick() {
 function handleMouseEnter(){
    // console.log("mouse is here");
     title.innerText = "Mouse is here!"
-}
+} 
 
 function handleMouseLeave(){
     title.innerText = "Mouse is gone";
-}
+} 
 
-title.addEventListener("click",handleTitleclick); //여기서 handleTitleclick뒤에 괄호()를 안쓰는건 정말 중요함 => 괄호쓰면 바로 실행 됨, 우리는 크릭이벤트가 발생하면 자바스크립트에서 자동으로 실행하기를 기대하는것임.
+title.onclick("click",handleTitleclick); //여기서 handleTitleclick뒤에 괄호()를 안쓰는건 정말 중요함 => 괄호쓰면 바로 실행 됨, 우리는 크릭이벤트가 발생하면 자바스크립트에서 자동으로 실행하기를 기대하는것임.
 title.addEventListener("mouseenter", handleMouseEnter);
 title.addEventListener("mouseleave", handleMouseLeave);
 
