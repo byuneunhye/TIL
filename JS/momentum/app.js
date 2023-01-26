7e4e7001db396a92f8c0dc21df879b54ad1919f5
byuneunhye <gracebyun06@gmail.com>
2023-01-26 18:23:46 +0900

    3.7 CSS in Javascript part Two

--- JS/momentum/app.js
+++ JS/momentum/app.js
@@ -1,16 +1,13 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handletitleclick(){
-    if(h1.className === "active"){
+    const clickedClass = "clicked"
+    if(h1.className === clickedClass ){
         h1.className = "";
     }else {
-        h1.className = "active";
+        h1.className = clickedClass;
     }
 }
 
 
 h1.addEventListener("click", handletitleclick); 
-
-//1. element를 찾아라
-//2. event를 listen해라
-//3. 그 event에 반응해라 
