023e3fddbb3a4600588cd01d474d8edbbce90e77
byuneunhye <gracebyun06@gmail.com>
2023-01-26 18:09:07 +0900

    3.7 중간커밋

--- JS/momentum/app.js
+++ JS/momentum/app.js
@@ -1,17 +1,16 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
-//console.dir(h1);
-
-function handletitleclick(){ 
-    const currentColor = h1.style.color;
-    let newColor;  //let, 새로 업데이트 될 수 있는 변수 선언 할 때 씀/ 값 할당 안 해줬으므로 비어있는 let변수 선언
-    if(currentColor === "blue"){
-        newColor = "tomato";
-    } else {
-        newColor = "blue";
+function handletitleclick(){
+    if(h1.className === "active"){
+        h1.className = "";
+    }else {
+        h1.className = "active";
     }
-    h1.style.color = newColor;
 }
 
 
 h1.addEventListener("click", handletitleclick); 
+
+//1. element를 찾아라
+//2. event를 listen해라
+//3. 그 event에 반응해라 
