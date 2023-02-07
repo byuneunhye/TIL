24f97a4480fae115db203d6a865bb1a9c2cee114
변은혜 <102288426+byuneunhye@users.noreply.github.com>
2023-02-07 13:10:55 +0900

    4.6 Loading Username

--- JS/momentum/app.js
+++ JS/momentum/app.js
@@ -3,15 +3,26 @@ const loginInput= document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault(); //브라우저의 기본동작을 막음 ex.form의 input에서 submit후 자동 새로고침
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem("username", username); //key
-    greeting.innerHTML = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username); //key
+    paintGreetings(username);
 }
 
-//콘솔창에 localStorage.setItem("username", "eh") 입력
-//웹브라우저의 loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME); 
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else {
+    paintGreetings(savedUsername) 
+}
