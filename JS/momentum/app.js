1e138398f6fe4525e864aded53e3ab4bda0b04ca
변은혜 <102288426+byuneunhye@users.noreply.github.com>
2023-02-07 12:40:19 +0900

    4.5 Saving Username

--- JS/momentum/app.js
+++ JS/momentum/app.js
@@ -8,11 +8,10 @@ function onLoginSubmit(event){
     event.preventDefault(); //브라우저의 기본동작을 막음 ex.form의 input에서 submit후 자동 새로고침
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    //greeting.innerHTML = "hello " + username; 
+    localStorage.setItem("username", username); //key
     greeting.innerHTML = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 //콘솔창에 localStorage.setItem("username", "eh") 입력
-//웹브라우저의 database 보기 : 개발자 도구 -> >> application -> Local Storage
-loginForm.addEventListener("submit", onLoginSubmit);
+//웹브라우저의 loginForm.addEventListener("submit", onLoginSubmit);
