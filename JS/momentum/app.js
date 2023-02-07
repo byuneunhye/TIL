7704568903fed466a3ac76fe3a6e8f5e28d24564
변은혜 <102288426+byuneunhye@users.noreply.github.com>
2023-02-07 12:02:07 +0900

    4.3 Events part Two

--- JS/momentum/app.js
+++ JS/momentum/app.js
@@ -1,9 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput= document.querySelector("#login-form input");
     
+const link = document.querySelector("a");
+
 function onLoginSubmit(event){
     event.preventDefault(); //브라우저의 기본동작을 막음 ex.form의 input에서 submit후 자동 새로고침
     console.log(loginInput.value);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit)
+function handleLinkClick(){
+    alert("clicked!");
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
