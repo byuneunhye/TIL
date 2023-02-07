1f5440450445ac21fb5df78fa069a9e9e9b77ab3
변은혜 <102288426+byuneunhye@users.noreply.github.com>
2023-02-07 12:25:52 +0900

    4.4 Getting Username

--- JS/momentum/app.js
+++ JS/momentum/app.js
@@ -1,17 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput= document.querySelector("#login-form input");
-    
-const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event){
     event.preventDefault(); //브라우저의 기본동작을 막음 ex.form의 input에서 submit후 자동 새로고침
-    console.log(loginInput.value);
-}
-
-function handleLinkClick(){
-    //event.preventDefault();
-    alert("clicked!");//alert는 좀 특이해서 html의 모든 기본동작을 막음 => 잘 사용 안 함
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    //greeting.innerHTML = "hello " + username; 
+    greeting.innerHTML = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
