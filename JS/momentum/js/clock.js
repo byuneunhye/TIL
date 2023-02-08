c70329baa1b51fba43c790c45b5d607789e21800
변은혜 <102288426+byuneunhye@users.noreply.github.com>
2023-02-08 23:10:44 +0900

    5.2 PadStart

--- JS/momentum/js/clock.js
+++ JS/momentum/js/clock.js
@@ -2,9 +2,14 @@ const clock = document.querySelector("h2#clock");
 
 function getClock(){
     const date = new Date();
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-}
+    const hours =  String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 
+
+    //clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
 //setTimeout(getClock, 5000); //setTimeout : interval처럼 반복하지는 않고 정한 시간 후에 실행하는거
 getClock(); //실행 하자마자 바로 시간 띄울 수 있음
 setInterval(getClock, 1000); 
