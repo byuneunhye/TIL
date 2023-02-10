42b06c0bd9ac740cad1f39680ea19bad005d99a1
변은혜 <102288426+byuneunhye@users.noreply.github.com>
2023-02-10 18:37:00 +0900

    7.1 Adding ToDos

--- JS/momentum/js/todo.js
+++ JS/momentum/js/todo.js
@@ -2,14 +2,22 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input"); //html전체가 아닌 for에서 input을 찾음
 const toDoList = document.getElementById("todo-list");
 
-//form이 자동으로 가지는 submit이벤트를 차단하는 함수를 만듦
-//submit의 기본동작은 페이지를 새로고침하는 것
+function paintTodo(newTodo){
+    //console.log("i will paint",newTodo);
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    console.log(li);
+    toDoList.appendChild(li); 
+}
 
 function handleToDoSubmit(event) { //submit이벤트 받아옴
     event.preventDefault();
     //console.log(toDoInput.value);
     const newTodo = toDoInput.value; //입력값 저장
     toDoInput.value = "";
+    paintTodo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
