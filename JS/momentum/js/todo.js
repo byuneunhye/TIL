4423c00adaac4f349962f45e4d4282b89734806e
변은혜 <102288426+byuneunhye@users.noreply.github.com>
2023-02-11 10:02:20 +0900

    7.2 Deleting To Dos

--- JS/momentum/js/todo.js
+++ JS/momentum/js/todo.js
@@ -2,16 +2,22 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input"); //html전체가 아닌 for에서 input을 찾음
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event){
+    //console.log(event); //mouseEvent가 떠야하는데 pointerEvent가 뜸... 왜지?
+    //console.dir(event.target.parentElement.innerText);
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintTodo(newTodo){
-    //console.log("i will paint",newTodo);
     const li = document.createElement("li");
     const span = document.createElement("span");
     span.innerText = newTodo;
     const button = document.createElement("button");
     button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
     li.appendChild(span);
     li.appendChild(button);
-    console.log(li);
     toDoList.appendChild(li); 
 }
 
