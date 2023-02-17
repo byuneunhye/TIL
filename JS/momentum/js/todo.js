8c09427b825a3d3eca93ae8f70a66bcf051b7a0b
변은혜 <102288426+byuneunhye@users.noreply.github.com>
2023-02-17 13:56:42 +0900

    7.6 Deleting To Dos part One

--- JS/momentum/js/todo.js
+++ JS/momentum/js/todo.js
@@ -12,13 +12,15 @@ function saveToDos(){
 
 function deleteToDo(event){
     const li = event.target.parentElement;
+    console.log(li.id);
     li.remove();
 }
 
 function paintTodo(newTodo){
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteToDo);
@@ -31,15 +33,19 @@ function handleToDoSubmit(event) { //submit이벤트 받아옴
     event.preventDefault();
     const newTodo = toDoInput.value; //입력값 저장
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintTodo(newTodo);
+    const newTodoObj = {
+        text:newTodo,
+        id: Date.now(),
+    };
+    toDos.push(newTodoObj);
+    paintTodo(newTodoObj);
     saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
-console.log(savedToDos);  //localStorage에 있는 todos의 값을 콘솔에 띄워보기, 아무것도 없다면 null
+//console.log(savedToDos);  //localStorage에 있는 todos의 값을 콘솔에 띄워보기, 아무것도 없다면 null
 if(savedToDos !== null){
     const parsedToDos = JSON.parse(savedToDos); 
     toDos = parsedToDos; //local저장소에 리스트를 추가하면 배열에 덮어쓰지 않고, 추가하게 해줌 . 즉, 기존에 저장 되어있던 배열값들이 새로고침해도, 새 리스트를 추가해도 남아 있을 수 있다는 것.
