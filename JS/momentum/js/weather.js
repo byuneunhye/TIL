812fc35ef9aa46832b04fb033a90e77fdfeba30f
변은혜 <102288426+byuneunhye@users.noreply.github.com>
2023-02-17 17:18:10 +0900

    8.1 Weather API

--- JS/momentum/js/weather.js
+++ JS/momentum/js/weather.js
@@ -1,10 +1,22 @@
+const API_KEY = "d93df3e18f4f043b97ce6445b196ce1b";
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
+    const lon = position.coords.longitude;
+    //console.log("You live in", lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`; 
+    //console.log(url);
+     fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
 }
 function onGeoError(){
     alert("Can't fint you. No weather for you");
-}
+}  
 
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError); //getCurrentPosition : success와 error을 받음 
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
